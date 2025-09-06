39867b4b245963c8da5a42c30d504395eced57ff
Ali Kamel <ali.kamel.ali.2004@gmail.com>
2025-09-07 00:45:25 +0300

    Complete script for adding book and Change form element attributes
    Complete script for adding a new book, except for a case where the user adds a book with a title that an existing book has.
    
    Additional:
    1. Set `autocomplete="on"` on all form input elements, except text input for book description.
       The feature is not working as expected for inputs for book title title and number of pages.
    
    2. Set `value="save"` for save buttons and `value="discard"` for discard buttons
    3. Remove object destructuring syntax from function `addBook` and make it return book added
    4. Create a separate function `displayBook` for displaying a single book given its `Book` object or its UUID, and move its script from function `displayAllBooks`
    4. Improve comments in markup and script files

--- script.js
+++ script.js
@@ -15,13 +15,6 @@ function Book(title, author, genre, pages, read, description) {
     Object.assign(this, { title, author, genre, pages, read, description });
 }
 
-// Object destructuring used in the function to allow the `Book` object to be correctly created, regardless of the order of properties in the object passed as argument.
-function addBook({ title, author, genre, pages, read, description }) {
-    const newBook = new Book(title, author, genre, pages, read, description);
-
-    allBooks.push(newBook);
-}
-
 // Contains all the books the user has added to their library, in addition to a starting example book.
 const allBooks = [
     // Starting Example book
@@ -35,22 +28,68 @@ const allBooks = [
     ),
 ];
 
+function addBook(title, author, genre, pages, read, description) {
+    const newBook = new Book(title, author, genre, pages, read, description);
+    allBooks.push(newBook);
+
+    return newBook;
+}
+
 const emptyLibraryPromptContainer = document.querySelector(".empty-library-prompt-container");
 const bookList = document.querySelector(".book-list");
 
+const addBookButtonContainers = [
+    document.querySelector(".header-right"),
+    document.querySelector(".library-section > .section-header"),
+    emptyLibraryPromptContainer.firstElementChild
+];
+
 // Appends the "Add Book" button to the elements of the document it is supposed to be a child of, and adds an event listener.
 function appendAddBookButton() {
-    // (1) Append the "Add Book" button
-
-    const addBookButtonContainers = [
-        document.querySelector(".header-right"),
-        document.querySelector(".library-section > .section-header"),
-        emptyLibraryPromptContainer.firstElementChild
-    ];
-
     const addBookButton = document.querySelector("#add-book-button-template").content.cloneNode(true);
     addBookButtonContainers.forEach(container => container.appendChild(addBookButton.cloneNode(true)));
 } appendAddBookButton();
+
+const bookCardTemplate = bookList.querySelector("#book-card-li-template");
+
+function displayBook(bookOrBookID) {
+    let book = undefined;
+
+    if (typeof bookOrBookID === "string")
+        book = allBooks.find(book => (book.id === bookOrBookID));
+    else if (bookOrBookID instanceof Book)
+        book = bookOrBookID;
+
+    if (book === undefined)
+        console.error(`Attempting to display an invalid or non-existent book.`);
+
+    const numberOfPagesUnknown = (book.pages === null);
+
+    const bookPages = numberOfPagesUnknown ? "Unknown number of pages" : `${book.pages} page${(book.pages === 1) ? "" : "s"}`;
+    const bookStatus = (book.read ? "read" : "unread");
+
+    const bookCard = bookCardTemplate.content.cloneNode(true);
+
+    bookCard.querySelector(".book-title > cite").textContent = book.title;
+    bookCard.querySelector(".book-author").textContent = book.author;
+    bookCard.querySelector(".book-genre").textContent = book.genre;
+    bookCard.querySelector(".number-of-pages").textContent = bookPages;
+    bookCard.querySelector(".book-description").textContent = book.description;
+    bookCard.querySelector(".book-status").textContent = bookStatus;
+
+    if (numberOfPagesUnknown)
+        bookCard.querySelector(".number-of-pages").style.fontStyle = "italic";
+
+    // TODO: Handle the cases where optional fields have no values.
+
+    const markAsReadButton = bookCard.querySelector(".mark-as-read-unread");
+    const markAsReadButtonTitleAndText = "mark as " + (book.read ? "un" : "") + "read";
+
+    markAsReadButton.setAttribute("title", markAsReadButtonTitleAndText);
+    markAsReadButton.textContent = markAsReadButtonTitleAndText;
+
+    bookList.appendChild(bookCard.cloneNode(true));
+}
 
 function displayAllBooks() {
     // (1) Return early if the library is empty
@@ -63,27 +102,7 @@ function displayAllBooks() {
         return;
 
     // (2) Display all book cards if the library is not empty
-    allBooks.forEach(book => {
-        const bookCard = bookList.querySelector("#book-card-li-template").content.cloneNode(true);
-
-        const bookPages = `${book.pages} page${(book.pages === 1) ? "" : "s"}`;
-        const bookStatus = (book.read ? "read" : "unread");
-
-        bookCard.querySelector(".book-title > cite").textContent = book.title;
-        bookCard.querySelector(".book-author").textContent = book.author;
-        bookCard.querySelector(".book-genre").textContent = book.genre;
-        bookCard.querySelector(".number-of-pages").textContent = bookPages;
-        bookCard.querySelector(".book-description").textContent = book.description;
-        bookCard.querySelector(".book-status").textContent = bookStatus;
-
-        const markAsReadButton = bookCard.querySelector(".mark-as-read-unread");
-        const markAsReadButtonTitleAndText = "mark as " + (book.read ? "un" : "") + "read";
-
-        markAsReadButton.setAttribute("title", markAsReadButtonTitleAndText);
-        markAsReadButton.textContent = markAsReadButtonTitleAndText;
-
-        bookList.appendChild(bookCard);
-    });
+    allBooks.forEach(book => displayBook(book));
 } displayAllBooks();
 
 document.addEventListener('click', (event) => {
@@ -95,6 +114,8 @@ document.addEventListener('click', (event) => {
 
     if (event.target.matches(".delete-book")) {
         // TODO
+
+        return;
     }
 });
 
@@ -105,16 +126,42 @@ document.addEventListener('click', (event) => {
         // Open the dialog as a modal.
         dialog.showModal();
 
+        const isAddBook = event.target.matches(".add-book");
+
         const dialogTitle = dialog.querySelector(".dialog-title");
-        dialogTitle.textContent = (event.target.matches(".add-book")) ? "Add New Book" : "Edit Book";
+        dialogTitle.textContent = isAddBook ? "Add New Book" : "Edit Book";
     }
 });
 
+let bookData = [];
+
 dialog.addEventListener('click', (event) => {
-    if (event.target.matches(".dialog-close, .dialog-close > *"))
-        // Close the dialog
-        dialog.close();
+    if (event.target.matches("[value='discard'], [value='discard'] *"))
+        dialog.close("noData");
+    else if (event.target.matches("[value='save'], [value='save'] *")) {
+        const dialogFormElements = Array.from(dialog.querySelectorAll("input, textarea"));
+        bookData = dialogFormElements.map((dialogFormElement) => {
+            if (dialogFormElement.type === "checkbox")
+                return (dialogFormElement.checked);
+
+            if (dialogFormElement.type === "number" && dialogFormElement.value === "")
+                return null;
+
+            return dialogFormElement.value;
+        });
+
+        // The dialog element will be automatically closed due to `method="dialog"` set on the `form` element.
+    }
 });
+
+dialog.addEventListener('close', () => {
+    if (dialog.returnValue === "noData")
+        return;
+
+    displayBook(addBook(...bookData));
+});
+
+// Script to update the number of characters left in the description textbox as the user modifies its content.
 
 const description = dialog.querySelector("#book-description");
 const numberOfCharactersLeft = dialog.querySelector(".number-of-characters");
